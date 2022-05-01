8a9ec3f56b0679afca89d2a543d0886acf14d091
Boierul <49337867+Boierul@users.noreply.github.com>
2022-05-01 14:55:26 +0200

    Added startup video in Main Activity

--- app/src/main/java/com/example/skateshopapp/MainActivity.java
+++ app/src/main/java/com/example/skateshopapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.skateshopapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.startupVideo);
+        button = findViewById(R.id.creteAccButton);
+
+        String path = "android.resource://com.example.skateshopapp/" + R.raw.sratuo_video_slowed;
+        Uri uri = Uri.parse(path);
+
+        videoView.setVideoURI(uri);
+        videoView.setOnPreparedListener(mediaPlayer -> {
+            mediaPlayer.start();
+        });
+
+        videoView.setOnCompletionListener(mediaPlayer -> mediaPlayer.start());
+
+        button.setOnClickListener(view ->
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class)));
     }
 }
