93f4a7c86f28aa4ecac00c30e12be146956969f6
Boierul <49337867+Boierul@users.noreply.github.com>
2022-05-11 15:44:22 +0200

    Navigation Drawer implemented.

--- app/src/main/java/com/example/skateshopapp/activity/HomePageActivity.java
+++ app/src/main/java/com/example/skateshopapp/activity/HomePageActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -38,6 +41,8 @@ public class HomePageActivity extends AppCompatActivity {
     private ItemViewModel itemViewModel;
 
     private ImageView home;
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -184,16 +189,30 @@ public class HomePageActivity extends AppCompatActivity {
         accessoriesRecycleAdapter.notifyDataSetChanged();
 
         /* ----------------------------------------------------------------------------------------------------------------------------- */
+        /* Navigation Drawer */
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.red_primary_darker));
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        /* ----------------------------------------------------------------------------------------------------------------------------- */
 
         // TODO setup button path (it opens multiple HomePages and this might cause problems)
-        home = findViewById(R.id.logo);
-        home.setOnClickListener(view -> {
-            if (this != HomePageActivity.this) {
-                /* TODO change back*/
-//                startActivity(new Intent(this, HomePageActivity.class));
-            }
-            startActivity(new Intent(this, ProfileActivity.class));
-        });
+//        home = findViewById(R.id.logo);
+//        home.setOnClickListener(view -> {
+//            if (this != HomePageActivity.this) {
+//                /* TODO change back*/
+////                startActivity(new Intent(this, HomePageActivity.class));
+//            }
+//            startActivity(new Intent(this, ProfileActivity.class));
+//        });
 
     }
 }
